9b09f28dacd9d2ab7c6d2952fc30768e5ebbf40b
Julik <me@julik.nl>
2011-06-10 17:28:51 +0200

    Start adding channel tests

--- test/test_channel.rb
+++ test/test_channel.rb
@@ -4,7 +4,19 @@ require File.dirname(__FILE__) + "/../lib/flame_channel_parser"
 D = 0.001
 
 class TestChannel < Test::Unit::TestCase
-  def test_fail
-    flunk "Not written yet"
+  def test_responds_to_array_methods
+    c = FlameChannelParser::Channel.new("b/c", "d", "f")
+    [:empty?, :length, :each, :[], :push, :<<].each {|m| assert c.respond_to?(m) }
   end
+  
+  def test_path_for_present_nodename
+    c = FlameChannelParser::Channel.new("position/x", "Axis", "axis1")
+    assert_equal "axis1/position/x", c.path
+  end
+  
+  def test_path_for_absent
+    c = FlameChannelParser::Channel.new("Frame/Frame", nil, nil)
+    assert_equal "Frame/Frame", c.path
+  end
+  
 end
